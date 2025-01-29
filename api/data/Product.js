3b750befae86297d9492e1bbf1638f6ec2873846
Arankarl <115096414+Dumgrammer@users.noreply.github.com>
2025-01-30 00:02:18 +0800

    Initial Push

---
+++ api/data/Product.js
@@ -0,0 +1,35 @@
+const products = [
+    {   
+        id: 1,
+        name: "MSI RTX 5090 Suprim Liquid X",
+        image: "https://media.overclock3d.net/2024/05/9fjSunEc-RTX-5090.jpg",
+        description: "Lorem ipsum",
+        price: 2000,
+        countInStock: 10,
+        rating: 4.5,
+        numReview: 45
+    },
+    {
+        name: "ROG STRIX B850-A GAMING WIFI",
+        image: "https://dlcdnwebimgs.asus.com/gain/8435EF24-6EF3-44AA-97B9-E6858ACD495C",
+        description: "Lorem anim anim",
+        price: 19.99,
+        countInStock: 5,
+        rating: 4.0,
+        numReview: 30
+    },
+    {
+        name: "ROG THOR 1600W Titanium III",
+        image: "https://dlcdnwebimgs.asus.com/files/media/c2161dd2-24f5-4160-9f13-81cecb73713b/v2/img/magnetic-oled-display.jpg",
+        description: "Lorem wow ow",
+        price: 39.99,
+        countInStock: 8,
+        rating: 4.8,
+        numReview: 50
+    }
+    // Add more products as needed
+];
+
+
+
+module.exports = Products;
