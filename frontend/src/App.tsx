eecd696be8db21e808590162bb1f0150a65136e9
Arankarl <115096414+Dumgrammer@users.noreply.github.com>
2025-02-04 03:15:54 +0800

    Small changes talo

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,6 +4,8 @@ import Product from "./components/Product";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import ProductDetail from "./views/ProductDetails";
 import Home from "./views/Home";
+import Login from "./views/Auth/Login";
+import Register from "./views/Auth/Register";
 
 function App() {
   return (
@@ -11,6 +13,8 @@ function App() {
     <Routes>
       <Route path="/" element={<Home/>}></Route>
       <Route path="/products/:id" element={<ProductDetail/>}></Route>
+      <Route path="/login" element={<Login/>} ></Route>
+      <Route path="/register" element={<Register/>}></Route>
     </Routes>
   </Router>
   );
