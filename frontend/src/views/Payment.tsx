aacdc9b28e8ffa42427eb9dec9e906ae712368f0
kwrkeiji <157571928+kwrkeiji@users.noreply.github.com>
2025-03-09 17:03:31 +0800

    Update Payment.tsx

--- frontend/src/views/Payment.tsx
+++ frontend/src/views/Payment.tsx
@@ -30,21 +30,21 @@ export default function Payment() {
                 <div className="mt-10 flex flex-col xl:flex-row jusitfy-center items-stretch w-full xl:space-x-8 space-y-4 md:space-y-6 xl:space-y-0">
                     <div className="flex flex-col justify-start items-start w-full space-y-4 md:space-y-6 xl:space-y-8">
                         <div className="flex flex-col justify-start items-start dark:bg-gray-800 bg-gray-50 px-4 py-4 md:py-6 md:p-6 xl:p-8 w-full">
-                            <p className="text-lg md:text-xl dark:text-white font-semibold leading-6 xl:leading-5 text-gray-800">Customer’s Cart</p>
+                            <p className="text-lg md:text-xl dark:text-white font-semibold leading-6 xl:leading-5 text-gray-800">Your Cart</p>
                             <div className="mt-4 md:mt-6 flex flex-col justify-start items-start w-full">
                                 {cart.length > 0 ? (
                                     cart.map((item, index) => (
-                                        <div key={index} className="pb-4 md:pb-8 w-full">
+                                        <div key={index} className="p-4 md:p-8 w-full bg-white flex flex-row gap-4 border-b border-gray-200">
                                             {/* Image block */}
-                                            <div className="flex flex-col w-full">
-                                                <img className="w-full hidden md:block" src={item.image} alt="dress" />
-                                                <img className="w-full md:hidden" src={item.image} alt="dress" />
+                                            <div className="flex flex-col">
+                                                <img className="m-auto h-[10vmin] hidden md:block " src={item.image} alt="dress" />
+                                                <img className="m-auto h-[10vmin] md:hidden" src={item.image} alt="dress" />
                                             </div>
 
                                             {/* Content block */}
-                                            <div className="border-b border-gray-200 flex flex-col justify-start items-start w-full pb-8 space-y-4">
-                                                <div className="w-full flex flex-col justify-start items-start space-y-8">
-                                                    <h3 className="text-xl dark:text-white xl:text-2xl font-semibold leading-6 text-gray-800">{item.name}</h3>
+                                            <div className=" flex flex-col justify-start items-start w-full">
+                                                <div className="w-full flex flex-col justify-start items-start space-y-4">
+                                                    <h3 className="dark:text-white text-md sm:text-sm font-semibold leading-6 text-[#0A9659]">{item.name}</h3>
                                                     <div className="flex justify-start items-start flex-col space-y-2">
                                                         <p className="text-sm dark:text-white leading-none text-gray-800">
                                                             <span className="dark:text-gray-400 text-gray-300">Quantity: </span> {item.quantity}
@@ -55,11 +55,11 @@ export default function Payment() {
                                                     </div>
                                                 </div>
 
-                                                <div className="flex justify-between space-x-8 items-start w-full">
-                                                    <p className="text-base dark:text-white xl:text-lg leading-6">$36.00</p>
-                                                    <p className="text-base dark:text-white xl:text-lg leading-6 text-gray-800">01</p>
-                                                    <p className="text-base dark:text-white xl:text-lg font-semibold leading-6 text-gray-800">₱3000</p>
-                                                </div>
+                                                {/* <div className="flex justify-between space-x-8 items-start w-full mt-auto">
+                                                    <p className="text-base dark:text-white xl:text-sm leading-6">$36.00</p>
+                                                    <p className="text-base dark:text-white xl:text-sm leading-6 text-gray-800">01</p>
+                                                    <p className="text-base dark:text-white xl:text-sm font-semibold leading-6 text-gray-800">₱3000</p>
+                                                </div> */}
                                             </div>
                                         </div>
                                     ))
@@ -71,9 +71,11 @@ export default function Payment() {
 
 
                         </div>
-                        <div className="flex justify-center flex-col md:flex-row flex-col items-stretch w-full space-y-4 md:space-y-0 md:space-x-6 xl:space-x-8">
-                            <div className="flex flex-col px-4 py-6 md:p-6 xl:p-8 w-full bg-gray-50 dark:bg-gray-800 space-y-6">
-                                <h3 className="text-xl dark:text-white font-semibold leading-5 text-gray-800">Summary</h3>
+                    </div>
+                    <div className="bg-gray-50 dark:bg-gray-800 w-full xl:w-96 flex items-center md:items-start py-2 md:p-2 xl:p-4 flex-col">
+                        {/* Summary */}
+                        <div className="flex flex-col px-4 py-6 md:p-6 xl:p-8 w-full bg-white dark:bg-gray-800 space-y-6 xl:mb-4">
+                                <h3 className="text-xl dark:text-white font-semibold leading-5 text-gray-800">Order Summary</h3>
                                 <div className="flex justify-center items-center w-full space-y-4 flex-col border-gray-200 border-b pb-4">
                                     <div className="flex justify-between w-full">
                                         <p className="text-base dark:text-white leading-4 text-gray-800">Subtotal</p>
@@ -85,53 +87,30 @@ export default function Payment() {
                                     <p className="text-base dark:text-gray-300 font-semibold leading-4 text-gray-600">₱3000</p>
                                 </div>
                             </div>
-                            <div className="flex flex-col justify-center px-4 py-6 md:p-6 xl:p-8 w-full bg-gray-50 dark:bg-gray-800 space-y-6">
+                            {/* Payment */}
+                            <div className="flex flex-col justify-center px-4 py-6 md:p-6 xl:p-8 w-full bg-white dark:bg-gray-800 space-y-6">
                                 <h3 className="text-xl dark:text-white font-semibold leading-5 text-gray-800">Payment</h3>
-                                <div className="flex justify-between items-start w-full">
+                                <div className="flex flex-row xl:flex-col justify-between items-start xl:items-end w-full">
                                     <div className="flex justify-center items-center space-x-4">
                                         <div className="w-8 h-8">
                                             <img className="w-full h-full" alt="logo" src="/Campus_Cart.png" />
                                         </div>
                                         <div className="flex flex-col justify-start items-center">
-                                            <p className="text-lg leading-6 dark:text-white font-semibold text-gray-800">Arrival of Orders<br /><span className="font-normal">Delivery within 3 months</span></p>
+                                            <p className="text-lg leading-6 dark:text-white font-semibold text-gray-800">Arrival of Orders<br />
+                                            <span className="font-normal">Delivery within 3 months</span></p>
                                         </div>
                                     </div>
-                                    <p className="text-lg font-semibold leading-6 dark:text-white text-gray-800">₱3300.00</p>
+                                    <p className="xl:mt-4 text-lg font-semibold leading-6 dark:text-white text-[#0A9659]">₱3300.00</p>
                                 </div>
-                                <div className="w-full flex justify-center items-center">
-                                    <button className="hover:bg-black dark:bg-white dark:text-gray-800 dark:hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-800 py-5 w-96 md:w-full bg-gray-800 text-base font-medium leading-4 text-white">
+                                <div className="w-full flex xl:flex-col gap-2 justify-center items-center">
+                                    <button className="hover:bg-[#0CA562] dark:bg-white dark:text-gray-800 dark:hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-800 py-3 rounded-sm w-96 md:w-full bg-[#0A9659] text-base font-medium leading-4 text-white">
+                                        Cash on Pickup
+                                    </button>
+                                    <button className="hover:bg-[#0CA562] focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-800 py-3 rounded-sm w-96 md:w-full bg-[#0A9659] text-base font-medium leading-4 text-white">
                                         Payment
                                     </button>
                                 </div>
                             </div>
-                        </div>
-                    </div>
-                    <div className="bg-gray-50 dark:bg-gray-800 w-full xl:w-96 flex justify-between items-center md:items-start px-4 py-6 md:p-6 xl:p-8 flex-col">
-                        <h3 className="text-xl dark:text-white font-semibold leading-5 text-gray-800">Customer</h3>
-                        <div className="flex flex-col md:flex-row xl:flex-col justify-start items-stretch h-full w-full md:space-x-6 lg:space-x-8 xl:space-x-0">
-                            <div className="flex flex-col justify-start items-start flex-shrink-0">
-
-                                <div className="flex justify-center text-gray-800 dark:text-white md:justify-start items-center space-x-4 py-4 border-b border-gray-200 w-full">
-                                    <img className="dark:hidden" src="/logo192.png" alt="email" />
-                                    <img className="hidden dark:block" src="/logo192.png" alt="email" />
-                                    <p className="cursor-pointer text-sm leading-5 ">appgradesolutions@gmail.com</p>
-                                </div>
-                            </div>
-                            <div className="flex justify-between xl:h-full items-stretch w-full flex-col mt-6 md:mt-0">
-                                <div className="flex justify-center md:justify-start xl:flex-col flex-col md:space-x-6 lg:space-x-8 xl:space-x-0 space-y-4 xl:space-y-12 md:space-y-0 md:flex-row items-center md:items-start">
-                                    <div className="flex justify-center md:justify-start items-center md:items-start flex-col space-y-4 xl:mt-8">
-                                        <p className="text-base dark:text-white font-semibold leading-4 text-center md:text-left text-gray-800">Yr - Block</p>
-                                        <p className="w-48 lg:w-full dark:text-gray-300 xl:w-48 text-center md:text-left text-sm leading-5 text-gray-600">Diyan lang</p>
-                                    </div>
-
-                                </div>
-                                <div className="flex w-full justify-center items-center md:justify-start md:items-start">
-                                    <button className="mt-6 md:mt-0 dark:border-white dark:hover:bg-gray-900 dark:bg-transparent dark:text-white py-5 hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-800 border border-gray-800 font-medium w-96 2xl:w-full text-base font-medium leading-4 text-gray-800">
-                                        Edit Details
-                                    </button>
-                                </div>
-                            </div>
-                        </div>
                     </div>
                 </div>
             </div>
