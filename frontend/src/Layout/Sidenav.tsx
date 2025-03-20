1512f1a9f03e731f2cd39256f0d27a130d8f035c
kwrkeiji <157571928+kwrkeiji@users.noreply.github.com>
2025-03-20 15:03:50 +0800

    Small change admin topnav
    - changed static photo to GC
    - populate some products

--- frontend/src/Layout/Sidenav.tsx
+++ frontend/src/Layout/Sidenav.tsx
@@ -25,7 +25,7 @@ export default function Sidenav() {
             <div>
               <button type="button" className="flex text-sm bg-gray-800 rounded-full focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600" aria-expanded="false" data-dropdown-toggle="dropdown-user">
                 <span className="sr-only">Open user menu</span>
-                <img className="w-8 h-8 rounded-full" src="https://flowbite.com/docs/images/people/profile-picture-5.jpg" alt="user photo"/>
+                <img className="w-8 h-8 rounded-full" src="/GC Logo.png" alt="user photo"/>
               </button>
             </div>
             <div className="z-50 hidden my-4 text-base list-none bg-white divide-y divide-gray-100 rounded-sm shadow-sm dark:bg-gray-700 dark:divide-gray-600" id="dropdown-user">
